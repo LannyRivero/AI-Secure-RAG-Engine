8c4da0cfac240f2a52dac3c84285d9c2a089217e
lannyrivero <lannyrivero288@gmail.com>
2026-02-27 21:03:45 +0100

    chore: initial Spring Boot skeleton (3.5.11, Java 21)

---
+++ src/test/java/com/lanny/ailab/AiLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanny.ailab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
