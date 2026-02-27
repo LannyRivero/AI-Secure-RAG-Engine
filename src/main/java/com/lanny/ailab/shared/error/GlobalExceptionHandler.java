bdeff7c2769712568c7b2dbdb4c43f85cbd31196
lannyrivero <lannyrivero288@gmail.com>
2026-02-27 23:37:06 +0100

    feat: implement GlobalExceptionHandler for handling validation errors and return ApiError response

---
+++ src/main/java/com/lanny/ailab/shared/error/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.lanny.ailab.shared.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleValidationException(MethodArgumentNotValidException ex) {
+
+        String errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ApiError apiError = new ApiError(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation Error",
+                errors);
+
+        return ResponseEntity.badRequest().body(apiError);
+    }
+}
