6deeb5ad9659039afb6b719dd8cdaeee792767e9
lannyrivero <lannyrivero288@gmail.com>
2026-03-16 19:41:49 +0100

    refactor(tests): rename test method for clarity and add integration tag

--- src/test/java/com/lanny/ailab/AiLabApplicationTests.java
+++ src/test/java/com/lanny/ailab/AiLabApplicationTests.java
@@ -1,13 +1,15 @@
 package com.lanny.ailab;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("integration")
 class AiLabApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void loads_application_context() {
 	}
 
 }
