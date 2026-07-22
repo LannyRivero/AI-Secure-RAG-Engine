0eadc9beb3498ea4212d9de7e7705ad0af62d057
LannyRivero <lannyrivero288@gmail.com>
2026-07-22 20:54:02 +0200

    feat: remove redundant index creation for business metadata in document_chunks

--- src/main/resources/db/migration/V9__add_business_metadata_to_document_chunks.sql
+++ src/main/resources/db/migration/V9__add_business_metadata_to_document_chunks.sql
@@ -7,21 +7,3 @@ ALTER TABLE document_chunks
     ADD COLUMN metadata_tags TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[],
     ADD COLUMN metadata_owner VARCHAR(100),
     ADD COLUMN metadata_classification VARCHAR(100);
-
-CREATE INDEX idx_chunks_tenant_document_type
-    ON document_chunks (tenant_id, metadata_document_type);
-
-CREATE INDEX idx_chunks_tenant_document_date
-    ON document_chunks (tenant_id, metadata_document_date);
-
-CREATE INDEX idx_chunks_tenant_source
-    ON document_chunks (tenant_id, metadata_source);
-
-CREATE INDEX idx_chunks_tenant_owner
-    ON document_chunks (tenant_id, metadata_owner);
-
-CREATE INDEX idx_chunks_tenant_classification
-    ON document_chunks (tenant_id, metadata_classification);
-
-CREATE INDEX idx_chunks_metadata_tags
-    ON document_chunks USING gin (metadata_tags);
